f31ad9abfbc745ce386da206c8b91e11d94096e7
Ben <benbperry1@gmail.com>
2021-12-14 14:41:30 -0800

    commit tree 04b1dd6ab6c58e7695a0a6aae5e461b1e4fa8200

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
-class Dog
+require 'pry'
 
+
+class Dog
+    attr_reader :name
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+      end
+    
+    #   def name
+    #     @this_dogs_name
+    #   end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
+binding.pry
